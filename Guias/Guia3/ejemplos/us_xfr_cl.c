31114de9e9f4e0b59ae35226915cf1e66b5f6284
JesusHuayhua <jmhuayhua@pucp.edu.pe>
2024-09-16 21:12:32 -0500

    server3 - client3
    Complete example server3 and client3

--- Guias/Guia3/ejemplos/us_xfr_cl.c
+++ Guias/Guia3/ejemplos/us_xfr_cl.c
@@ -17,17 +17,21 @@ int main(int argc, char *argv[]){
     ssize_t numRead;
     char buf[BUF_SIZE];
 
-    sfd = socket(AF_UNIX, SOCK_STREAM,0);
-    if(sfd == -1)
+    sfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sfd == -1)
         perror("socket");
 
-    memset(&addr, 0, sizeof(struct sockaddr_un));
+    memset(&addr,0,sizeof(struct sockaddr_un));
     addr.sun_family = AF_UNIX;
     strncpy(addr.sun_path, SV_SOCK_PATH, sizeof(addr.sun_path) - 1);
-    if(connect(sfd, (struct sockaddr *) &addr,
-        sizeof(struct sockaddr_un)) == -1)
+    if (connect(sfd, (struct sockaddr *) &addr,
+        sizeof(struct sockaddr_un)) == -1) {
         perror("connect");
-    while(numRead == -1)
+    }
+    while( (numRead = read(STDIN_FILENO,buf,BUF_SIZE)) > 0)
+        if (write(sfd, buf, numRead) != numRead)
+            perror("partial/failed write");
+    if(numRead == -1)
         perror("read");
     exit(EXIT_SUCCESS);
 }
--- Guias/Guia3/ejemplos/us_xfr_sv.c
+++ Guias/Guia3/ejemplos/us_xfr_sv.c
@@ -3,44 +3,53 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <time.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
 
 #define SV_SOCK_PATH "/tmp/us_xfr"
-#define BUF_SIZE 100
-#define BACKLOG 5
 
+#define BUF_SIZE 100
+
+#define BACKLOG 5
 
 int main(int argc, char *argv[]){
     struct sockaddr_un addr;
-    int sfd, cfd;
+    int sfd,cfd;
     ssize_t numRead;
     char buf[BUF_SIZE];
-    
+
     sfd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (sfd == -1)
-        perror("scoket");
-    
+        perror("socket");
     if (remove(SV_SOCK_PATH) == -1 && errno != ENOENT)
         perror("remove");
 
     memset(&addr,0,sizeof(struct sockaddr_un));
     addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, SV_SOCK_PATH, sizeof(addr.sun_path) - 1);
-    
-    if (bind(sfd, (struct sockaddr *) &addr, sizeof(struct sockaddr_un)) == -1)
+    strncpy(addr.sun_path, SV_SOCK_PATH, sizeof(addr.sun_path));
+
+    if (bind(sfd, (struct sockaddr *)&addr,sizeof(struct sockaddr_un)) == -1)
+        perror("bind");
+
+    if (listen(sfd,BACKLOG) == -1) {
         perror("listen");
-    for(;;){
-        cfd = accept(sfd,NULL, NULL);
-        if (cfd == -1)
-            perror("accept");
-        while((numRead = read(cfd, buf, BUF_SIZE)) > 0)
-            if (write(STDOUT_FILENO,buf,numRead) != numRead)
-                perror("partial/failed write");
-        if (numRead == -1)
-            perror("read");
-        if(close(cfd) == -1)
-            perror("close");
     }
+    for ( ;  ;  ) {
+        cfd = accept(sfd, NULL, NULL);
+        if(cfd == -1)
+            perror("accept");
+        while( (numRead = read(cfd, buf, BUF_SIZE)) > 0)
+            if(write(STDOUT_FILENO, buf, numRead) != numRead)
+                perror("partial/failed write");
+        if (numRead == -1) {
+            perror("read");
+        }
+        if (close(cfd) == -1) {
+            perror("close");
+        }
+    }
+    exit(EXIT_SUCCESS);
 }
